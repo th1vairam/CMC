ad10042c61b7a9589343dd5dc72edaae601d4ee5
Ben Logsdon <aeberhart@gmail.com>
2015-03-30 12:58:03 -0700

    update job script

--- myjobscript.sh
+++ myjobscript.sh
@@ -1,1 +1,9 @@
-mpirun Rscript buildMpiSparrowNet.R "testData.csv" 2 "/shared/CMC/CMCSPARROW"
+#! /bin/bash
+#$ -S /bin/bash
+#$ -V
+#$ -cwd
+#$ -N Job1
+#$ -pe orte 2
+#$ -j y
+#$ -l h_rt=00:30:00
+mpirun -np 2 Rscript buildMpiSparrowNet.R "testData.csv" 2 "/shared/CMC/CMCSPARROW"
