cf91029be7dc157151291f9538747cd2f77b474c
Ben Logsdon <aeberhart@gmail.com>
2015-03-30 08:15:50 -0700

    function to simulate data from a simple random network model and save it to a file

---
+++ simulation.R
@@ -0,0 +1,31 @@
+###Simulate small data-set
+
+n <- as.numeric(commandArgs(TRUE)[[1]])
+p <- as.numeric(commandArgs(TRUE)[[2]])
+prop <- as.numeric(commandArgs(TRUE)[[3]])
+file <- as.character(commandArgs(TRUE)[[4]])
+
+require(MASS)
+LAM <- matrix(rnorm(p^2)*rbinom(p^2,1,prop),p,p)
+diag(LAM) <- 1;
+OM <- LAM%*%t(LAM)
+
+Y <- mvrnorm(n,rep(0,p),solve(OM))
+
+rn <- sapply(1:n,function(i){return(paste(sample(letters,5,replace=T),collapse=''))})
+cn <- sapply(1:p,function(i){return(paste(sample(letters,5,replace=T),collapse=''))})
+
+while(sum(duplicated(rn))>0){
+  k <- sum(duplicated(rn))
+  rn[duplicated(rn)] <- sapply(1:k,function(i){return(paste(sample(letters,5,replace=T),collapse=''))})
+}
+
+while(sum(duplicated(cn))>0){
+  k <- sum(duplicated(cn))
+  cn[duplicated(cn)] <- sapply(1:k,function(i){return(paste(sample(letters,5,replace=T),collapse=''))})
+}
+
+rownames(Y) <- rn
+colnames(Y) <- cn
+
+write.csv(Y,file=file,quote=F)
