2b8ffac79ecf96ff0b3a9e2483d017d16a6bb71d
Ben Logsdon <aeberhart@gmail.com>
2015-03-31 12:58:05 -0700

    grab sva cmc data

--- cmcsva.R
+++ cmcsva.R
@@ -16,5 +16,19 @@ SVATFExpression <- synGet('syn2757149')
 cmcSVATFExpression <- read.delim(SVATFExpression@filePath)
 
 #combine gene expression data
+cmcExpression <- rbind(cmcSVATFExpression,cmcSVAGeneExpression)
 
+#download metadata
+syncmcMetaData <- synGet('syn2299154')
+metadata <- read.delim(syncmcMetaData@filePath,sep=',',stringsAsFactors=FALSE)
+
+metadata_freeze = metadata[-which(metadata$DLPFC_RNA_report..Exclude. == 1), ]
+
+cmcExpressionFreeze <- cmcExpression[,which(colnames(cmcExpression)%in%as.character(metadata_freeze$DLPFC_RNA_isolation..Sample.RNA.ID))]
+
+scz_samples <- metadata_freeze$DLPFC_RNA_isolation..Sample.RNA.ID[which(metadata_freeze$Dx == "SCZ")]
+
+cmcExpressionFreezeSCZ <- scale(data.matrix(cmcExpressionFreeze[,colnames(cmcExpressionFreeze)%in%scz_samples]))
+
+#extract schizophrenia cases only
 #
