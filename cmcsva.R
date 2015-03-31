8fab6668b94b1c800b347c58c52b54dfa6ddb01f
Ben Logsdon <aeberhart@gmail.com>
2015-03-31 13:23:26 -0700

    function to build cmc data-sets for network AWS network reconstrution pipeline

--- cmcsva.R
+++ cmcsva.R
@@ -1,34 +1,38 @@
 #script to grab SVA CMC data
 
+sczOutSva <- as.character(commandArgs(TRUE)[[1]])
+controlOutSva <- as.character(commandArgs(TRUE)[[2]])
+sczOut <- as.character(commandArgs(TRUE)[[3]])
+controlOut <- as.character(commandArgs(TRUE)[[4]])
+
 require(synapseClient)
 synapseLogin()
 
-SVAGeneExpressionSummary <- synGet('syn2757151')
-cmcSVAGeneSummary <- read.delim(SVAGeneExpressionSummary@filePath)
 
-SVATFExpressionSummary <- synGet('syn2757153')
-cmcSVATFSummary <- read.delim(SVATFExpressionSummary@filePath)
-
-SVAGeneExpression <- synGet('syn2757147')
-cmcSVAGeneExpression <- read.delim(SVAGeneExpression@filePath)
-
-SVATFExpression <- synGet('syn2757149')
-cmcSVATFExpression <- read.delim(SVATFExpression@filePath)
-
-#combine gene expression data
-cmcExpression <- rbind(cmcSVATFExpression,cmcSVAGeneExpression)
-
-#download metadata
-syncmcMetaData <- synGet('syn2299154')
-metadata <- read.delim(syncmcMetaData@filePath,sep=',',stringsAsFactors=FALSE)
-
-metadata_freeze = metadata[-which(metadata$DLPFC_RNA_report..Exclude. == 1), ]
-
-cmcExpressionFreeze <- cmcExpression[,which(colnames(cmcExpression)%in%as.character(metadata_freeze$DLPFC_RNA_isolation..Sample.RNA.ID))]
-
-scz_samples <- metadata_freeze$DLPFC_RNA_isolation..Sample.RNA.ID[which(metadata_freeze$Dx == "SCZ")]
-
-cmcExpressionFreezeSCZ <- scale(data.matrix(cmcExpressionFreeze[,colnames(cmcExpressionFreeze)%in%scz_samples]))
-
-#extract schizophrenia cases only
-#
+makeDataMatrices <- function(geneSyn,tfSyn,sczFile,contFile,bpFile){
+  SVAGeneExpression <- synGet('syn2757147')
+  cmcSVAGeneExpression <- read.delim(SVAGeneExpression@filePath)
+  
+  SVATFExpression <- synGet('syn2757149')
+  cmcSVATFExpression <- read.delim(SVATFExpression@filePath)
+  
+  #combine gene expression data
+  cmcExpression <- rbind(cmcSVATFExpression,cmcSVAGeneExpression)
+  
+  #download metadata
+  syncmcMetaData <- synGet('syn2299154')
+  metadata <- read.delim(syncmcMetaData@filePath,sep=',',stringsAsFactors=FALSE)
+  
+  metadata_freeze = metadata[-which(metadata$DLPFC_RNA_report..Exclude. == 1), ]
+  
+  cmcExpressionFreeze <- cmcExpression[,which(colnames(cmcExpression)%in%as.character(metadata_freeze$DLPFC_RNA_isolation..Sample.RNA.ID))]
+  
+  scz_samples <- metadata_freeze$DLPFC_RNA_isolation..Sample.RNA.ID[which(metadata_freeze$Dx == "SCZ")]
+  
+  control_samples <- metadata_freeze$DLPFC_RNA_isolation..Sample.RNA.ID[which(metadata_freeze$Dx == "Control")]
+  
+  #extract schizophrenia cases only
+  cmcExpressionFreezeSCZ <- scale(t(data.matrix(cmcExpressionFreeze[,colnames(cmcExpressionFreeze)%in%scz_samples])))
+  
+  write.csv(cmcExpressionFreezeSCZ,file=file,quote=FALSE)
+}
