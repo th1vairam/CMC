608a5e998b9b748d39097d1cb91a11d0211720da
Ben Logsdon <aeberhart@gmail.com>
2015-03-31 14:32:57 -0700

    save results to synapse

--- grabCMCdata.sh
+++ grabCMCdata.sh
@@ -2,3 +2,4 @@
 
 Rscript makeDataMatrices.R "syn2757138" "syn2757140" "sczData.csv" "controlData.csv" "bpData.csv"
 Rscript makeDataMatrices.R "syn2757147" "syn2757149" "sczDataSVA.csv" "controlDataSVA.csv" "bpDataSVA.csv"
+
