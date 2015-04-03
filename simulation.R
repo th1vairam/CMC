073f77d6ffbcda44be598282f8632444083f7627
Ben Logsdon <aeberhart@gmail.com>
2015-04-03 16:02:50 -0700

    script to test pipelines...

--- simulation.R
+++ simulation.R
@@ -10,6 +10,8 @@ LAM <- matrix(rnorm(p^2)*rbinom(p^2,1,prop),p,p)
 diag(LAM) <- 1;
 OM <- LAM%*%t(LAM)
 
+
+
 Y <- mvrnorm(n,rep(0,p),solve(OM))
 
 rn <- sapply(1:n,function(i){return(paste(sample(letters,5,replace=T),collapse=''))})
@@ -28,4 +30,10 @@ while(sum(duplicated(cn))>0){
 rownames(Y) <- rn
 colnames(Y) <- cn
 
+rownames(OM) <- cn
+colnames(OM) <- cn
+
 write.csv(Y,file=file,quote=F)
+write.csv(OM,file='truth.csv',quote=F)
+cat('syn3157146','syn3157148',file='syn.txt',sep='\n')
+cat('https://github.com/blogsdon/CMC/blob/master/simulation.R','')
---
+++ testpipelinesim.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#simulate some data
+
+Rscript simulation 500 500 0.006 "data.csv"
+qsub buildNet.sh -d "data.csv" -slrgt -n 16
+qsub buildNet.sh -d "data.csv" -aw
+./pushNet.sh -p "syn3500049" -c code.txt -g syn.txt -sawlrgt -v "none" -o "Homo zapiens" -d "bonitis" -u "blood"
