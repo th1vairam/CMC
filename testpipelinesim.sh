23ed249b7615e64c0088303836144e68cf92210c
Ben Logsdon <aeberhart@gmail.com>
2015-04-06 10:23:32 -0700

    fixing bugs

--- testpipelinesim.sh
+++ testpipelinesim.sh
@@ -4,6 +4,6 @@
 #simulate some data
 
 Rscript simulation.R 500 500 0.006 "data.csv"
-qsub buildNet.sh -d "data.csv" -slrgt -n 16
-qsub buildNet.sh -d "data.csv" -aw
+qsub buildNet.sh -d "data.csv" -slrgt -n 16 -p "/shared/metanetworkSynapse/"
+qsub buildNet.sh -d "data.csv" -aw -p "/shared/metanetworkSynapse/"
 ./pushNet.sh -p "syn3500049" -c code.txt -g syn.txt -sawlrgt -v "none" -o "Homo zapiens" -d "bonitis" -u "blood"
