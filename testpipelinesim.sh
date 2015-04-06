dcbb205f1b3fbd0149d05fea5c516b128b1c3d57
Ben Logsdon <aeberhart@gmail.com>
2015-04-06 15:18:57 -0700

    updated simulation scripts

--- testpipelinesim.sh
+++ testpipelinesim.sh
@@ -5,5 +5,10 @@
 
 Rscript simulation.R 100 100 0.02 "data.csv"
 qsub buildNet.sh -d "data.csv" -slrgt -n 16 -p "/shared/metanetworkSynapse/"
+#qsub buildNet.sh -d "data.csv" -slrgt -n 16 -p "/shared/metanetworkSynapse/"
+#qsub buildNet.sh -d "data.csv" -slrgt -n 16 -p "/shared/metanetworkSynapse/"
+#qsub buildNet.sh -d "data.csv" -slrgt -n 16 -p "/shared/metanetworkSynapse/"
+#qsub buildNet.sh -d "data.csv" -slrgt -n 16 -p "/shared/metanetworkSynapse/"
+#qsub buildNet.sh -d "data.csv" -slrgt -n 16 -p "/shared/metanetworkSynapse/"
 qsub buildNet.sh -d "data.csv" -aw -p "/shared/metanetworkSynapse/"
 ./pushNet.sh -p "syn3500049" -c code.txt -b syn.txt -sawlrgt -v "none" -o "HomoZapiens" -d "bonitis" -u "blood"
