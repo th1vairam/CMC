d003dd453bbaaf35062f033af1e9c781c0e7e4ca
Ben Logsdon <aeberhart@gmail.com>
2015-04-12 12:30:57 -0700

    fix names, add library

--- ensemblToGeneId.R
+++ ensemblToGeneId.R
@@ -1,6 +1,7 @@
+require(biomaRt)
 ensembl=useMart("ensembl")
 ensembl = useDataset("hsapiens_gene_ensembl",mart=ensembl)
-queryBioMart <- function(x,ensembl){
+ensemblToGeneId <- function(x,ensembl){
   gene1 <- NA;
   try(gene1 <- getBM(attributes='external_gene_name',filters='ensembl_gene_id',values=x,mart=ensembl),silent=T)
   if(is.null(gene1)){
