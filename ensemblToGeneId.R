32896e0c87ec26a5eab93188748ff1d3c02574fe
Ben Logsdon <aeberhart@gmail.com>
2015-04-15 08:27:47 -0700

    fix errors in fxn

--- ensemblToGeneId.R
+++ ensemblToGeneId.R
@@ -1,11 +1,23 @@
-require(biomaRt)
-ensembl=useMart("ensembl")
-ensembl = useDataset("hsapiens_gene_ensembl",mart=ensembl)
-ensemblToGeneId <- function(x,ensembl){
-  gene1 <- NA;
-  try(gene1 <- getBM(attributes='external_gene_name',filters='ensembl_gene_id',values=x,mart=ensembl),silent=T)
-  if(is.null(gene1)){
-    gene1<-NA
+
+ensemblToGeneId <- function(ensemblId,dataSet='hsapiens_gene_ensembl'){
+  ensemblToGeneIdInternal <- function(x,ensembl){
+    gene1 <- NA;
+    try(gene1 <- getBM(attributes='external_gene_name',filters='ensembl_gene_id',values=x,mart=ensembl),silent=T)
+    if(is.null(gene1)){
+      gene1<-NA
+    }
+    if(length(unlist(gene1))==0){
+      gene1=NA
+    }
+    return(gene1)
   }
-  return(gene1)
+  require(dplyr)
+  require(biomaRt)
+  ensembl=useMart("ensembl")
+  ensembl = useDataset(dataSet,mart=ensembl)
+  geneId <- ensemblId %>%
+    sapply(ensemblToGeneIdInternal,ensembl) %>%
+    unlist() %>%
+    as.character()
+  return(geneId)
 }
