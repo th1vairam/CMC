3b1997475a991dd2c9b27b1a98ffdf91637f9319
Ben Logsdon <baelogsdon@gmail.com>
2015-06-18 22:00:45 -0700

    update networks

--- pushCMCcontrolNetworks.sh
+++ pushCMCcontrolNetworks.sh
@@ -9,7 +9,8 @@
 
 
 #controls
-/shared/metanetworkSynapse/pushNet.sh -a "syn3526290" -b "/shared/CMC/codeControl.txt" -c "/shared/CMC/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "Control" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
+#/shared/metanetworkSynapse/pushNet.sh -a "syn3526290" -b "/shared/CMC/codeControl.txt" -c "/shared/CMC/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "Control" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
+/shared/metanetworkSynapse/pushNet.sh -a "syn3526290" -b "/shared/CMC/codeControl.txt" -c "/shared/CMC/syn.txt" -mv -r "None" -s "HomoSapiens" -t "Control" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
 
 #cases
 #/shared/metanetworkSynapse/pushNet.sh -a "syn3526289" -b "/shared/CMC/code.txt" -c "/shared/CMC/syn.txt" -dev -r "None" -s "HomoSapiens" -t "Schizophrenia" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
