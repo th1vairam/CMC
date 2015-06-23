42777b2d459dcce406cd244200ea0fe9f52a072f
Thanneer Malai Perumal <thanneer.perumal@sagebase.org>
2015-06-22 21:25:36 -0700

    network properties calculation script simplified

--- networkProperties.R
+++ networkProperties.R
@@ -7,42 +7,33 @@ networkProperties <- function(g,  # graph for which network properties are to be
   results <- list()
   
   # Density: Shows how sparse or dense a network is, for full network the value is 1 and for empty network the value is NaN(0/0)
-  if (any(metrics %in% 'density')){  
-    density <- igraph::graph.density(g)
-    results$density = density
+  # Centralisation: Measures how well network has a star-like topology (closer to 1 more likely the topology is star-like)
+  # Average node degree: Average degree of all nodes in network
+  if (any(metrics %in% c('density','centralisation', 'avgNodeDegree'))){
+    results$totalDegree = igraph::graph.strength(g)
+    
+    nVertex = igraph::vcount(g)    
+    results$maxDegree = max(results$totalDegree)
+    
+    results$avgNodeDegree = mean(results$totalDegree)
+    results$density <- 2*sum(E(g)$weight)/(nVertex*(nVertex-1))
+    results$centralisation <- nVertex/(nVertex - 2) * (results$maxDegree/(nVertex - 1) - results$density)
   }
   
   # Diameter: Maximum distance between all the vertex in network
-  if (any(metrics %in% 'diameter')){  
-    diameter <- igraph::diameter(g)
-    results$diameter = diameter
-  }
-  
-  # Average node degree: Average degree of all nodes in network
-  if (any(metrics %in% 'avgNodeDegree')){  
-    avgNodeDegree <- mean(igraph::degree(g))
-    results$avgNodeDegree = avgNodeDegree
-  }
-  
   # Average path length: Average distance between any two vertex in network
-  if (any(metrics %in% 'avgPathLength')){  
-    avgPathLength <- igraph::average.path.length(g)
-    results$avgPathLength = avgPathLength
+  if (any(metrics %in% c('diameter','avgPathLength'))){  
+    results$shortestPaths <- igraph::shortest.paths(g)
+    results$diameter <- max(results$shortestPaths)
+    results$avgPathLength <- mean(results$shortestPaths)
   }
   
   # Average clustering coefficeint: Gives first hand information on network clusters
   if (any(metrics %in% 'avgClusteringCoefficient')){  
-    V(g)$ClusteringCoefficeint <- igraph::transitivity(g,
-                                                       v=V(g),
-                                                       type = 'barrat')
-    avgClusteringCoefficient <- mean(V(g)$ClusteringCoefficeint,na.rm=T)
-    results$avgClusteringCoefficient = avgClusteringCoefficient
-  }
-  
-  # Centralisation: Measures how well network has a star-like topology (closer to 1 more likely the topology is star-like)
-  if (any(metrics %in% 'centralisation')){  
-    centralisation <- igraph::vcount(g)/(igraph::vcount(g) - 2) * (max(igraph::degree(g))/(igraph::vcount(g) - 1) - igraph::graph.density(g))
-    results$centralisation = centralisation
+    results$ClusteringCoefficeint <- igraph::transitivity(g,
+                                                          vids=V(g),
+                                                          type = 'barrat')
+    results$avgClusteringCoefficient <- mean(results$ClusteringCoefficeint,na.rm=T)
   }
   
   return(results)
