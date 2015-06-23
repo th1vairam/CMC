b4b99c180298e4d49eb4f888f98ed600da2bcfdd
Thanneer Malai Perumal <thanneer.perumal@sagebase.org>
2015-06-22 21:27:06 -0700

    node ranking script modified to include weighted degree calculations and estimated betweenness calculation based on cutoff

--- nodeRankings.R
+++ nodeRankings.R
@@ -1,6 +1,6 @@
 #### Function to measure different node ranking metrics ####
 nodeRankings <- function(g, # graph for which network properties are to be calculated (in igraph format)
-                         metrics = 'totalDegree' # character vector of network properties, 
+                         metrics = 'totalDegree', # character vector of network properties, 
 # Other options are:
     # inDegree
     # outDegree
@@ -11,46 +11,44 @@ nodeRankings <- function(g, # graph for which network properties are to be calcu
     # eigen
     # pageRank
     # nearNeighbor
-                         ){
+                         cutoff = 2){
     
   results <- data.frame(row.names=V(g)$name)
   
   # In degree
   if (any(metrics %in% 'inDegree')){
     print('Calculating in degree...')
-    results$inDegree <- igraph::degree(g,
-                                       v=V(g), 
-                                       mode = 'in', 
-                                       loop = T,
-                                       normalized = T)
+    results$inDegree <- igraph::graph.strength(g,
+                                               vids=V(g), 
+                                               mode = 'in', 
+                                               loops = F)
   }
   
   # Out degree
   if (any(metrics %in% 'outDegree')){
     print('Calculating out degree...')
-    results$outDegree <- igraph::degree(g,
-                                        v=V(g), 
-                                        mode = 'out', 
-                                        loop = T,
-                                        normalized = T)
+    results$outDegree <- igraph::strength(g,
+                                          vids=V(g),
+                                          mode = 'out',
+                                          loops = F)
   }
   
   # Total degree: An important node is involved in a large number of interactions
   if (any(metrics %in% 'totalDegree')){
     print('Calculating total degree...')
-    results$totalDegree <- igraph::degree(g,
-                                          v=V(g), 
-                                          mode = 'all', 
-                                          loop = T,
-                                          normalized = T)
+    results$totalDegree <- igraph::strength(g,
+                                            vids=V(g),
+                                            mode = 'all',
+                                            loops = F)
   }
   
   # Betwenness centrality: An important node will lie on a high proportion of paths between other nodes in the network.
   if (any(metrics %in% 'betwenness')){
     print('Calculating betweenness...')
-    results$betweenness <- igraph::betweenness(g,
-                                               v= V(g),
-                                               normalized = T)
+    results$betweenness <- igraph::betweenness.estimate(g,
+                                                        vids = V(g),
+                                                        directed = F,
+                                                        cutoff = cutoff)
   }
   
   # Clustering coefficeint
@@ -79,7 +77,8 @@ nodeRankings <- function(g, # graph for which network properties are to be calcu
   if (any(metrics %in% 'eigen')){
     print('Calculating eigen centrality...')
     results$eigen <- igraph::alpha.centrality(g,
-                                              nodes=V(g))
+                                              nodes=V(g),
+                                              sparse=T)
   }
   
   # Closeness centrality: An important node is typically “close” to, and can communicate quickly with, the other nodes in the network
